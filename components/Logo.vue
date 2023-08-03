c864980d8af4310eccd14eb5ee252cfd614e9d0a
Will Blair <85643015+willblair0708@users.noreply.github.com>
2023-08-03 09:38:52 -0700

    Add files via upload

---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="/KP.png" alt="Kleiner Perkins Fellows" class="logo" />
+  </div>
+</template>
+
+<style lang="ts" scoped>
+css({
+  div: {
+    height: 'inherit',
+  },
+  '.logo': {
+    width: '400px', /* Adjust the width as per your preference */
+    height: 'auto', /* Maintain the aspect ratio */
+  },
+});
+</style>
---
+++ tokens.config.ts
@@ -0,0 +1,18 @@
+import { defineTheme } from 'pinceau';
+
+export default defineTheme({
+  color: {
+    primary: {
+      50: '#F5F8FA',
+      100: '#DCE7EE',
+      200: '#0C9FC1',
+      300: '#97AEC5',
+      400: '#7491B1',
+      500: '#0074DB', // Beautiful, professional, excellent shade of blue
+      600: '#0C9FC1',
+      700: '#0C9FC1',
+      800: '#0074DB',
+      900: '#091725'
+    }
+  }
+});
